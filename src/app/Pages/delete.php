a467b35df7bd2f72ebc9f525feae1723f5fb3d27
DESKTOP-GO1SN2Q\shrik <jeanmichelsoler44@gmail.com>
2021-03-18 17:01:27 +0100

    add grey on delete

--- src/app/Pages/delete.php
+++ src/app/Pages/delete.php
@@ -4,15 +4,15 @@ include("../HeadFoot/header.php");
 <div class="row">
     <ul class="collapsible popout">
         <li>
-            <div class="collapsible-header center-align">
+            <div class="collapsible-header center-align grey">
                 <span class="card-title ">
                     <p>Sacs à Dos</p>
                     <p class="reduc red-text">-25%</p>
                     <button class="supp red waves-effect waves-light btn" href="#">Supprimer</button>
                 </span>
-
+x
             </div>
-            <div class="collapsible-body">
+            <div class="collapsible-body grey">
                 <img class="image" src="../../../images/bug.png">
                 <p class="description">Offre spéciale pour la rentrée des classes.</p>
                 <p class="red-text">Expire le : Date de fin de l'offre</p>
