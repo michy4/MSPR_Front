4eb219cdc634b232966a6913b507f0089d842069
RENOU Florent <florent.renou01@gmail.fr>
2021-03-19 17:16:34 +0100

    Ajout UsersPromotions

--- src/app/JS/maria_db.js
+++ src/app/JS/maria_db.js
@@ -23,9 +23,13 @@ const query = (queryStr, values) => {
 
 const getPromotionsDB = () => query('select * from promotion')
 const getUsersDB = () => query('select * from utilisateur')
+const getUsersPromotionsDB = () => query('select * from utilisateur_promotion')
+
 
 const findPromotionsDB = (id) => query('select * from promotion where id=?', [id]).then(res => res[0])
 const findUsersDB = (id) => query('select * from utilisateur where id=?', [id]).then(res => res[0])
+const findUsersPromotionsDB = (utilisateur_id, promotion_id) => query('select * from utilisateur_promotion where utilisateur_id=? and promotion_id=?', [utilisateur_id,promotion_id]).then(res => res[0])
+
 
 const replacePromotionsDB = (id, newpromotion) => {
     return query('update promotion set nom=?, promotion=?, categorie_id=?, nb_utilisation=?, image=?, date_expiration=?, description=?, utilisateur_id=? where id=?',
@@ -37,6 +41,12 @@ const replaceUsersDB = (id, newuser) => {
         [newuser.nom, newuser.prenom, newuser.email, newuser.motDePasse, newuser.admin, id])
         .then(() => findUsersDB(newuser.id))
 }
+const replaceUsersPromotionsDB = (utilisateur_id, promotion_id, newuserpromotion) => {
+    return query('update utilisateur_promotion set nb_utilisation=? where utilisateur_id=? and promotion_id=?',
+        [newuserpromotion.nb_utilisation, newuserpromotion.utilisateur_id, newuserpromotion.promotion_id])
+        .then(() => findUsersPromotionsDB(newuserpromotion.utilisateur_id,newuserpromotion.promotion_id))
+}
+
 
 const insertPromotionsDB = (promotion) => {
     const newpromotion = Object.assign({}, promotion, { id: uuidv4() })
@@ -44,15 +54,23 @@ const insertPromotionsDB = (promotion) => {
         [newpromotion.id, newpromotion.nom, newpromotion.promotion, newpromotion.categorie_id, newpromotion.nb_utilisation, newpromotion.image, newpromotion.date_expiration, newpromotion.description, newpromotion.utilisateur_id])
         .then(() => findPromotionsDB(newpromotion.id))
 }
-const insertUsersDB = (promotion) => {
-    const newuser = Object.assign({}, promotion, { id: uuidv4() })
+const insertUsersDB = (user) => {
+    const newuser = Object.assign({}, user, { id: uuidv4() })
     return query('insert into utilisateur (id, nom, prenom, email, motDePasse, admin) values (?, ?, ?, ?, ?, ?)',
         [newuser.id, newuser.nom, newuser.prenom, newuser.email, newuser.motDePasse, newuser.admin])
         .then(() => findUsersDB(newuser.id))
 }
+const insertUsersPromotionsDB = (UsersPromotions) => {
+    const newuserpromotion = Object.assign({}, UsersPromotions)
+    return query('insert into utilisateur_promotion (utilisateur_id, promotion_id, nb_utilisation) values (?, ?, ?)',
+        [newuserpromotion.utilisateur_id, newuserpromotion.promotion_id, newuserpromotion.nb_utilisation])
+        .then(() => findUsersPromotionsDB(newuserpromotion.utilisateur_id,newuserpromotion.promotion_id))
+}
+
 
 const deletePromotionsDB = (id) => query('delete from promotion where id=?', [id])
 const deleteUsersDB = (id) => query('delete from utilisateur where id=?', [id])
+const deleteUsersPromotionsDB = (utilisateur_id, promotion_id) => query('delete from utilisateur_promotion where utilisateur_id=? and promotion_id=?', [utilisateur_id, promotion_id])
 
 module.exports = {
     getPromotionsDB,
@@ -64,5 +82,10 @@ module.exports = {
     findUsersDB,
     replaceUsersDB,
     insertUsersDB,
-    deleteUsersDB
+    deleteUsersDB,
+    getUsersPromotionsDB,
+    findUsersPromotionsDB,
+    replaceUsersPromotionsDB,
+    insertUsersPromotionsDB,
+    deleteUsersPromotionsDB
 }
